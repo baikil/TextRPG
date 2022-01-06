09f244c91ca99523d7764e4379bbb7553f459ead
baikil <78218286+baikil@users.noreply.github.com>
2022-01-06 16:16:58 -0500

    Add files via upload

--- TextRPGinstaller.bat
+++ TextRPGinstaller.bat
@@ -1,3 +1,11 @@
 @echo off
 title TextRPG installer
+echo.
 echo Installing TextRPG ...
+echo.
+set "txtrpgdir=%public%\textrpg"
+set "url=https://github.com/baikil/TextRPG/raw/main/music.mp3"&set "file=%txtrpgdir%\music.mp3"&call :dl
+set "url=https://raw.githubusercontent.com/baikil/TextRPG/main/TextRPG.bat"&set "file=%txtrpgdir%\TextRPG.bat"&call :dl
+:dl
+powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('%url%','%file%')"
+exit /b

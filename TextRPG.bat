710248d84502b618776bc3ac3127e72778b61ed2
baikil <78218286+baikil@users.noreply.github.com>
2022-01-06 16:23:15 -0500

    Add files via upload

--- TextRPG.bat
+++ TextRPG.bat
@@ -3,11 +3,12 @@
 title TextRPG ^| Loading...
 set "txtrpgdir=%public%\textrpg"
 call :audio %txtrpgdir%\music.mp3
-ping -n 11 127.0.0.1>nul
+echo Press any key to exit this epic demo
+pause >nul
 taskkill /im wmplayer.exe /f
 exit
 :audio
 echo CreateObject("Wscript.Shell").Run "wmplayer /play /close ""%1""", 0, False > audio.vbs
-ping -n 4 127.0.0.1>nul
+ping -n 2 127.0.0.1>nul
 start audio.vbs
 exit /b
--- TextRPGinstaller.bat
+++ TextRPGinstaller.bat
@@ -6,6 +6,8 @@ echo.
 set "txtrpgdir=%public%\textrpg"
 set "url=https://github.com/baikil/TextRPG/raw/main/music.mp3"&set "file=%txtrpgdir%\music.mp3"&call :dl
 set "url=https://raw.githubusercontent.com/baikil/TextRPG/main/TextRPG.bat"&set "file=%txtrpgdir%\TextRPG.bat"&call :dl
+start "%txtrpgdir%\TextRPG.bat"
+exit
 :dl
 powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('%url%','%file%')"
 exit /b
